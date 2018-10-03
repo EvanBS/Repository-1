aced245a9c2d423c08ffe6660d0c5e82f336f653
EvanBS <evandiesp@gmail.com>
2018-10-03 19:52:48 +0300

    Добавьте файлы проекта.

---
+++ EC0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EC0
+{
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("that works");
+            List<int> numbers = new List<int>() {1, 2, 3};
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine($"number : {number}");
+            }
+            
+
+        }
+
+    }
+
+}
