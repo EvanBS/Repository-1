63b3944a69c101dd7f3b7423279a7eb3b782dda2
EvanBS <evandiesp@gmail.com>
2018-10-04 18:04:42 +0300

    Array Reverse

--- EC0/Program.cs
+++ EC0/Program.cs
@@ -6,11 +6,30 @@ namespace EC0
     
     class Program
     {
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
         public static void Main(string[] args)
         {
-            // tripled
-            string s = Console.ReadLine();
-            Console.WriteLine($"double : {Convert.ToInt32(s) * 3}");
+
+            int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                Swap(ref arr[i], ref arr[arr.Length - i - 1]);
+            }
+            
+            foreach (int i in arr)
+            {
+                Console.Write($"{i} \t");
+            }
         }
 
     }
